f63dd832976ca6a2bd5fa7af910b15c7d4c45d22
kanade <jrdrywnt.163.com>
2024-11-21 13:56:27 +0800

    init commit

---
+++ init.sql
@@ -0,0 +1,9 @@
+INSERT INTO `book` (`bid`, `bookName`, `des`, `price`) VALUES (6, 'dada', 'kosaka', 114514);
+INSERT INTO `book` (`bid`, `bookName`, `des`, `price`) VALUES (7, 'dsfs', 'fdg', 22);
+INSERT INTO `borrow` (`id`, `sid`, `bid`) VALUES (8, 4, 6);
+INSERT INTO `borrow` (`id`, `sid`, `bid`) VALUES (9, 4, 7);
+INSERT INTO `student` (`sid`, `name`, `gender`, `age`) VALUES (3, 'kaoli', '女', 18);
+INSERT INTO `student` (`sid`, `name`, `gender`, `age`) VALUES (4, 'slfj', '女', 1);
+INSERT INTO `student` (`sid`, `name`, `gender`, `age`) VALUES (5, '0d00', '女', 2);
+INSERT INTO `student` (`sid`, `name`, `gender`, `age`) VALUES (6, 'slfjssss', '女', 1);
+INSERT INTO `usermanager` (`id`, `username`, `password`, `nickname`) VALUES (1, 'admin', '123456', 'admin');
